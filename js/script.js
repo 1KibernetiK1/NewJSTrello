cf0c9920bfc18c249309559c572be20c0e2571ee
1KibernetiK1 <@rbiu.local>
2021-10-08 17:28:51 +0500

    test

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btnCreateList = document.querySelector(".btn");
+const desk = document.querySelector(".new_desk");
+
+btnCreateList.addEventListener("click", function(){
+    let list = document.createElement('div');
+    let listName = document.getElementById("list-name").nodeValue;
+    list.innerHTML = listName;
+});
